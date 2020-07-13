e8c2e4f4ddc288a4450d9819867d985269066bd0
José Ignacio <nachomas21@gmail.com>
2020-07-12 23:50:06 -0300

    por terminar tp 7

--- jmascherpa UNIDAD 7.cpp
+++ jmascherpa UNIDAD 7.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 class Ficha{
 private:
-	
-public:
 	char figura;
+public:
+	
 	Ficha();
 	Ficha(char);
 	char getFicha();
@@ -55,6 +55,8 @@ bool Ficha::figuraO(){
 	
 	
 	
+	
+	
 	if(figura=='O'){
 		return true;
 	}else {
@@ -65,16 +67,17 @@ bool Ficha::figuraO(){
 
 class Tablero{
 private:
-	Ficha mapa[3][3];
+	
 public:
+	Ficha mapa[3][3];
 	Tablero();
-	Ficha F1;
-	void definirFicha(int x,int y, char c);
+	Ficha F;
+	void ponerFicha(int x,int y, char c);
 	void dibujarTablero();
 	void gestionJuego();
 	void numerosDisponibles();
 	bool ganadorTateti();
-	
+	bool tableroCompleto();
 };
 Tablero::Tablero(){}
 
@@ -89,7 +92,7 @@ void Tablero::dibujarTablero(){
 	}
 }
 
-void Tablero::definirFicha(int x,int y, char c){
+void Tablero::ponerFicha(int x,int y, char c){
 	
 	mapa[x][y] = c;
 	
@@ -98,36 +101,63 @@ bool Tablero::ganadorTateti(){
 	
 	if((mapa[0][0].figuraX()) && (mapa[1][1].figuraX())&& (mapa[2][2].figuraX())){
 		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;
-	}else if((mapa[0][2].figuraX()) && (mapa[1][1].figuraX())&& (mapa[2][0].figuraX())){
+	}
+	if((mapa[0][2].figuraX()) && (mapa[1][1].figuraX())&& (mapa[2][0].figuraX())){
 		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;
-	}else if((mapa[0][0].figuraX()) && (mapa[1][0].figuraX())&& (mapa[2][0].figuraX())){
-		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;//Vertical
-	}else if((mapa[0][1].figuraX()) && (mapa[1][1].figuraX())&& (mapa[2][1].figuraX())){
-		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;//Vertical
-	}else if((mapa[0][2].figuraX()) && (mapa[1][2].figuraX())&& (mapa[2][2].figuraX())){
-		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;//Vertical
-	}else if((mapa[0][0].figuraX()) && (mapa[0][1].figuraX())&& (mapa[0][2].figuraX())){
-		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;//HORIZONTAL
-	}else if((mapa[1][0].figuraX()) && (mapa[1][1].figuraX())&& (mapa[1][2].figuraX())){
-		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;//Vertical
-	}else if((mapa[0][2].figuraX()) && (mapa[2][1].figuraX())&& (mapa[2][2].figuraX())){
-		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;//Vertical
-	}else if((mapa[0][0].figuraO()) && (mapa[1][1].figuraO())&& (mapa[2][2].figuraO())){
+	}
+	if((mapa[0][0].figuraX()) && (mapa[1][0].figuraX())&& (mapa[2][0].figuraX())){
+		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;
+	}
+	if((mapa[0][1].figuraX()) && (mapa[1][1].figuraX())&& (mapa[2][1].figuraX())){
+		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;
+	}
+	if((mapa[0][2].figuraX()) && (mapa[1][2].figuraX())&& (mapa[2][2].figuraX())){
+		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;
+	}
+	if((mapa[0][0].figuraX()) && (mapa[0][1].figuraX())&& (mapa[0][2].figuraX())){
+		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;
+	}
+	if((mapa[1][0].figuraX()) && (mapa[1][1].figuraX())&& (mapa[1][2].figuraX())){
+		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;
+	}
+	if((mapa[0][2].figuraX()) && (mapa[2][1].figuraX())&& (mapa[2][2].figuraX())){
+		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;
+	}
+	if((mapa[2][0].figuraX()) && (mapa[2][1].figuraX()) && (mapa[2][2].figuraX())){
+		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;
+	}
+	if((mapa[2][2].figuraX()) && (mapa[1][2].figuraX()) && (mapa[0][2].figuraX())){
+		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;
+	}
+	if((mapa[2][0].figuraX()) && (mapa[1][1].figuraX()) && (mapa[0][2].figuraX())){
+		cout<<"Ganador del tateti es el jugador 1."<<endl;return true;
+	}
+	if((mapa[0][0].figuraO()) && (mapa[1][1].figuraO())&& (mapa[2][2].figuraO())){
 		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;
-	}else if((mapa[0][2].figuraO()) && (mapa[1][1].figuraO())&& (mapa[2][0].figuraO())){
+	}
+	if((mapa[0][2].figuraO()) && (mapa[1][1].figuraO())&& (mapa[2][0].figuraO())){
 		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;
-	}else if((mapa[0][0].figuraO()) && (mapa[1][0].figuraO())&& (mapa[2][0].figuraO())){
-		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;//Vertical
-	}else if((mapa[0][1].figuraO()) && (mapa[1][1].figuraO())&& (mapa[2][1].figuraO())){
-		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;//Vertical
-	}else if((mapa[0][2].figuraO()) && (mapa[1][2].figuraO())&& (mapa[2][2].figuraO())){
-		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;//Vertical
-	}else if((mapa[0][0].figuraO()) && (mapa[0][1].figuraO())&& (mapa[0][2].figuraO())){
-		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;//HORIZONTAL
-	}else if((mapa[1][0].figuraO()) && (mapa[1][1].figuraO())&& (mapa[1][2].figuraO())){
-		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;//Vertical
-	}else if((mapa[0][2].figuraO()) && (mapa[2][1].figuraO())&& (mapa[2][2].figuraO())){
-		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;//Vertical
+	}
+	if((mapa[0][0].figuraO()) && (mapa[1][0].figuraO())&& (mapa[2][0].figuraO())){
+		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;
+	}
+	if((mapa[0][1].figuraO()) && (mapa[1][1].figuraO())&& (mapa[2][1].figuraO())){
+		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;
+	}
+	if((mapa[0][2].figuraO()) && (mapa[1][2].figuraO())&& (mapa[2][2].figuraO())){
+		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;
+	}
+	if((mapa[0][0].figuraO()) && (mapa[0][1].figuraO())&& (mapa[0][2].figuraO())){
+		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;
+	}
+	if((mapa[1][0].figuraO()) && (mapa[1][1].figuraO())&& (mapa[1][2].figuraO())){
+		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;
+	}
+	if((mapa[0][2].figuraO()) && (mapa[2][1].figuraO())&& (mapa[2][2].figuraO())){
+		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;
+	}
+	if((mapa[2][0].figuraO()) && (mapa[2][1].figuraO())&& (mapa[2][2].figuraO())){
+		cout<<"Ganador del tateti es el jugador 2."<<endl;return true;
 	}
 	
 	return false;
@@ -137,43 +167,78 @@ bool Tablero::ganadorTateti(){
 void Tablero::numerosDisponibles(){
 	
 	
-	if((mapa[0][0].verificarFicha())==true){
+	if((mapa[0][0].verificarFicha())){
 		cout<<"Presione 1 para colocar ficha en (0,0)"<<endl;  
 	}
-	if((mapa[0][1].verificarFicha())==true){
+	if((mapa[0][1].verificarFicha())){
 		cout<<"Presione 2 para colocar ficha en (0,1)"<<endl;  
 	}
-	if((mapa[0][2].verificarFicha())==true){
+	if((mapa[0][2].verificarFicha())){
 		cout<<"Presione 3 para colocar ficha en (0,2)"<<endl;  
 	}
-	if((mapa[1][0].verificarFicha())==true){
+	if((mapa[1][0].verificarFicha())){
 		cout<<"Presione 4 para colocar ficha en (1,0)"<<endl;  
 	}
-	if((mapa[1][1].verificarFicha())==true){
+	if((mapa[1][1].verificarFicha())){
 		cout<<"Presione 5 para colocar ficha en (1,1)"<<endl;  
 	}
-	if((mapa[1][2].verificarFicha())==true){
+	if((mapa[1][2].verificarFicha())){
 		cout<<"Presione 6 para colocar ficha en (1,2)"<<endl;  
 	}
-	if((mapa[2][0].verificarFicha())==true){
+	if((mapa[2][0].verificarFicha())){
 		cout<<"Presione 7 para colocar ficha en (2,0)"<<endl;  
 	}
-	if((mapa[2][1].verificarFicha())==true){
+	if((mapa[2][1].verificarFicha())){
 		cout<<"Presione 8 para colocar ficha en (2,1)"<<endl;  
 	}
-	if((mapa[2][2].verificarFicha())==true){
+	if((mapa[2][2].verificarFicha())){
 		cout<<"Presione 9 para colocar ficha en (2,2)"<<endl;  
 	}
+	/*if(!ganadorTateti()){
+	cout<<"Elija una opcion: "<<endl;
+	
+	}*///PREGUNTAR PROFE
 	
 }
 
-void Tablero::gestionJuego(){
+
+
+bool Tablero::tableroCompleto(){
+	int contador = 0;
+	for(int i = 0;i<3;i++){
+		for(int j=0;j<3;j++){
+			if((mapa[i][j].figuraX())|| (mapa[i][j].figuraO())){
+				contador++;
+			}
+		}
+	}
+	if(contador==9){
+		return true;
+	}
+	
+	return false;
+	
+}
+class Juego{
+	
+	private:
+	public:
+	Juego();
+	Tablero T;
+	Ficha F;
+	void gestionJuego();
+};
+
+Juego::Juego(){
+	
+}
+void Juego::gestionJuego(){
 	int cont=0;
 	bool gameon = false;
-	numerosDisponibles();
+	T.numerosDisponibles();
 	while(gameon==false){
 		int x,y;
-		
+		int contGanador=0;
 		if(kbhit()){
 			int tecla=getch();
 			switch(tecla){
@@ -216,37 +281,42 @@ void Tablero::gestionJuego(){
 				break;
 			default:break;     
 			}
+			
 			if(cont%2==0){
-				if(mapa[x][y].verificarFicha()==true){
-					definirFicha(x,y,'X');
+				if((T.mapa[x][y]).verificarFicha()){
+					T.ponerFicha(x,y,'X');
 					cont++;
 				}else{
 					cout<<"Ese lugar ya esta ocupado, elija otro numero."<<endl;
 				}
 			}else if(cont%2!=0){
-				if(mapa[x][y].verificarFicha()==true){ 
-					definirFicha(x,y,'O');
+				if((T.mapa[x][y]).verificarFicha()){ 
+					T.ponerFicha(x,y,'O');
 					cont++;
 				}else{
 					cout<<"Ese lugar ya esta ocupado, elija otro numero."<<endl;
 				}
 			}
 			cout<<endl;
-			dibujarTablero();
+			T.dibujarTablero();
 			cout<<endl;
 			
 			cout<<"/////////////////////////////////"<<endl;
-			if(cont%2==0){
+			if(T.ganadorTateti()){
+				contGanador++;
+				break;
+			}
+			if((T.tableroCompleto())&& contGanador==0 ){
+				cout<<"Nadie gano :("<<endl;break;
+			}
+			
+			if((contGanador==0)&&(cont%2==0)){
 				cout<<"Le toca al jugador 1: X"<<endl;
-			}else if(cont%2!=0){
+			}else if((contGanador==0)&&(cont%2!=0)){
 				cout<<"Le toca al jugador 2: O"<<endl;
 			}
 			
-			numerosDisponibles();
-			
-			if(ganadorTateti()){
-				break;
-			}
+			T.numerosDisponibles();
 			
 		}
 		
@@ -255,13 +325,18 @@ void Tablero::gestionJuego(){
 }
 
 int main (int argc, char *argv[]) {
+	cout<<"JUEGO DE TA-TE-TI"<<endl;
+	cout<<"Jugador 1 X"<<endl;
+	cout<<"Jugador 2 O"<<endl;
 	
+	cout<<"Le toca al jugador 1: X"<<endl;
+	cout<<endl;
 	Tablero T;
-	Ficha F1;
+	Juego J;
 	T.dibujarTablero();
 	cout<<endl;
 	cout<<"     "<<"//////////////"<<endl;
-	T.gestionJuego();
+	J.gestionJuego();
 	
 	
 	
